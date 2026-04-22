48ace818f67f94b6018af5a92be0e465886a5fa0
Blenduca Suporte <suporte@blenduca.com.br>
2026-04-22 00:07:45 -0300

    fix: switch to HashRouter for reliable GitHub Pages deployment and remove workflow

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router";
+import { HashRouter, Routes, Route } from "react-router";
 import App from "./app/App.tsx";
 import Links from "./app/Links.tsx";
 import "./styles/index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename={import.meta.env.BASE_URL}>
+  <HashRouter>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/links" element={<Links />} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
